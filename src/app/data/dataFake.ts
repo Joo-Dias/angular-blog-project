4107efdc64cf53665b92d72104696d23ad840235
João Dias <joaovitorelf@gmail.com>
2023-09-06 18:46:54 -0300

    Deixando a content-page dinamica

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,8 +1,26 @@
 export const dataFake = [
   {
-    "id": 1,
-    "photo": "",
-    "title": "",
-    "description": "",
+    "id": "1",
+    "photo": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=1200&aspectRatio=1.78&format=jpeg",
+    "title": "Homem de Ferro",
+    "description": "Marvel anuncia novo home de ferro!",
+  },
+  {
+    "id": "2",
+    "photo": "https://disneyplusbrasil.com.br/wp-content/uploads/2023/02/Homem-Aranha-2002.jpg",
+    "title": "Homem Aranha faz sucesso!",
+    "description": "Homem-Aranha bla bla bla",
+  },
+  {
+    "id": "3",
+    "photo": "https://static-assets.bamgrid.com/product/disneyplus/images/share-default.14fadd993578b9916f855cebafb71e62.png",
+    "title": "Preço da Disney+ aumentou!",
+    "description": "A partir de ______ o proço da Disney+ irá aumentar!",
+  },
+  {
+    "id": "4",
+    "photo": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/05/Hulk-Vingadores.jpg",
+    "title": "Novo filme do hulk!",
+    "description": "Hulk blablablabla",
   }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { dataFake } from 'src/app/data/dataFake';
+
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
@@ -10,15 +12,24 @@ export class ContentComponent implements OnInit {
   photoCover:string = "";
   contentTitle:string = "";
   contentDescription:string = "";
+  private id:string | null = "0";
 
   constructor(private route:ActivatedRoute) {
 
   }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(value => {
-      console.log(value.get("id"));
-    })
+    this.route.paramMap.subscribe(value => this.id = value.get("id"));
+    this.setValueToComponents(this.id);
+  }
+
+  setValueToComponents(id:string | null){
+    const result = dataFake.filter(content => content.id == id)[0];
+
+    this.contentTitle = result.title;
+    this.contentDescription = result.description;
+    this.photoCover = result.photo;
+
   }
 
 }
